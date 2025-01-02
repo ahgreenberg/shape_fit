036d4298ca53243b35c0aec53b6295171ba04d67
Adam Greenberg <adam@greenhodge.net>
2025-01-01 21:01:10 -0800

    making spin angle differentiable

--- driver.py
+++ driver.py
@@ -20,23 +20,37 @@ num_mode = 30
 truth_model = utl.generate_gaussian_model(num_voxel, num_mode)
 
 # define observational parameters
-angles = tc.linspace(0,tc.pi,10)
-axis = tc.Tensor([0,0,1])
+angle_rate = tc.pi
+times = tc.linspace(0,1,10)
+
+axis = tc.Tensor([1,0,0])
 noise = 0.05
 
 # simulate measurements - 4E5 total pixels
 images = [utl.observe_model(utl.rotate_model(truth_model, axis, angle), noise) \
-          for angle in angles]
+          for angle in angle_rate*times]
 
 # recover model via fit - 8E6 total free model parameters
 tic = tm.time()
-num_epoch = 75
+num_epoch = 50
 cand_model = tc.zeros((num_voxel,num_voxel,num_voxel), requires_grad=True)
-optimizer = tc.optim.Adam([cand_model], lr=4E-2)
+cand_angle_rate = tc.ones((1,), requires_grad=True)
+
+# tc.no_grad context manager necessary to keep cand_angle_rate as a valid leaf 
+# node
+with tc.no_grad(): cand_angle_rate *= 2.5
+
+# define parameter groups to enable to different learning rates for fit-for 
+# variables
+params = [{'params': cand_model,      'lr': 5E-2}, 
+          {'params': cand_angle_rate, 'lr': 5E-3}]
+optimizer = tc.optim.Adam(params)
+
+# run optimization loop
 for k in range(num_epoch):
     optimizer.zero_grad()
-    loss = utl.loss_function(images, cand_model, axis, angles)
-    loss.backward()
+    loss = utl.loss_function(times, images, cand_model, cand_angle_rate, axis)
+    loss.backward(retain_graph=True)
     optimizer.step()
     print(f"Iteration: {k:3.0f}, Loss: {loss.item():8.1f}")
 
@@ -44,4 +58,4 @@ cand_model.detach_().round_()
 print(f"Total time: {tm.time()-tic:.1f} s")
 
 outfile = 'outputs/comparison.png'
-plt.show_comparison(images, cand_model, axis, angles, outfile)
+plt.show_comparison(images, cand_model, axis, times*cand_angle_rate, outfile)
--- src/utilities.py
+++ src/utilities.py
@@ -7,6 +7,7 @@ Created on Thu Dec 26 09:35:04 2024
 """
 
 import torch as tc
+from torch.nn import functional as fn
 
 def generate_gaussian_model(num_voxel, num_mode, 
                             thresh=5E-2, std_max=0.2, mean_max=0.5):
@@ -28,75 +29,47 @@ def generate_gaussian_model(num_voxel, num_mode,
     return (space>thresh).to(dtype=tc.float)
 
 def rotate_model(model, axis, angle):
-    # apply a continuous rotation to a voxel model
     
-    num_voxel = model.shape[0]    
+    # initialize helper variables for affine transformation matrix definition
     ax = axis/tc.linalg.norm(axis, ord=2)
-    cos = tc.cos(angle)
+    cos,sin = tc.cos(angle), tc.sin(angle)
     ncos = 1-cos
-    sin = tc.sin(angle)
+    
+    # initialize arguments for grid functions
+    grid_args = {'align_corners':False}
+    grid_shape = (1,1)+tuple(model.shape)
     
     # via https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-    R = tc.Tensor([[ax[0]**2*ncos+cos, ax[0]*ax[1]*ncos-ax[2]*sin, ax[0]*ax[2]*ncos+ax[1]*sin], \
-                   [ax[0]*ax[1]*ncos+ax[2]*sin, ax[1]**2*ncos+cos, ax[1]*ax[2]*ncos-ax[0]*sin], \
-                   [ax[0]*ax[2]*ncos-ax[1]*sin, ax[1]*ax[2]*ncos+ax[0]*sin, ax[2]**2*ncos+cos]])
+    # (this weird construction is necessary to maintain autograd graph)
+    zero = tc.zeros(1)[0]
+    R1 = tc.stack((ax[0]**2*ncos+cos, ax[0]*ax[1]*ncos-ax[2]*sin, ax[0]*ax[2]*ncos+ax[1]*sin,zero))
+    R2 = tc.stack((ax[0]*ax[1]*ncos+ax[2]*sin, ax[1]**2*ncos+cos, ax[1]*ax[2]*ncos-ax[0]*sin,zero))
+    R3 = tc.stack((ax[0]*ax[2]*ncos-ax[1]*sin, ax[1]*ax[2]*ncos+ax[0]*sin, ax[2]**2*ncos+cos,zero))
+    R = tc.stack((R1,R2,R3))
     
-    # define array coordinate system
-    indexing = tc.arange(num_voxel)
-    xgrid, ygrid, zgrid = tc.meshgrid((indexing, indexing, indexing))
-    grids = (xgrid.flatten(), ygrid.flatten(), zgrid.flatten())
-    coords = tc.stack(grids).to(dtype=tc.int64)
+    # use torch's interpolating (differentiable) matrix affine transformation
+    grid = fn.affine_grid(R[None,:,:], grid_shape, **grid_args) 
+    model_rot = fn.grid_sample(model[None,None,:,:,:], grid, **grid_args)
     
-    # apply rotation to coordinate system
-    coords_rot = (tc.matmul(R, coords-num_voxel/2)+num_voxel/2).round().to(dtype=tc.int64)
-    
-    # discard coordinates outside of array bounds
-    mask_lo = coords_rot>=0
-    mask_hi = coords_rot<num_voxel
-    mask = tc.all(tc.logical_and(mask_lo, mask_hi), dim=0)
-    coords = coords[:,mask].T
-    coords_rot = coords_rot[:,mask].T
-    
-    # apply rotated coordinate system
-    model_rot = tc.zeros(model.shape).to(dtype=model.dtype)
-    model_rot[coords[:,0],coords[:,1],coords[:,2]] \
-        = model[coords_rot[:,0],coords_rot[:,1],coords_rot[:,2]]
-        
-    return model_rot
+    return model_rot[0,0,:,:,:]
     
 def observe_model(model, noise = 0):
+    
     image = model.sum(dim=0)/model.shape[0]
     image += tc.normal( tc.zeros(image.shape), noise)
     return image
 
-def loss_function(images, model, axis, angles):
-    assert(len(angles)==len(images))
-        
+def loss_function(times, images, model, angle_rate, axis):
+    assert(len(times)==len(images))
+            
     # initialize loss with continuous penalty function for non-physical 
     # occupancy values (basically just a reciprocal super-gaussian)
     center,width = 0.5,0.5
-    loss = tc.sum( tc.exp(((model-center)/width/1.5)**4)-1 )/1E3
+    loss = tc.sum( tc.exp(((model-center)/width/1.5)**8)-1 )/1E3
     
     # loss is sum-squared residuals between synthetic and measured images
-    for angle,image in zip(angles, images):
+    for image,angle in zip(images, angle_rate*times):
         image_sim = observe_model(rotate_model(model, axis, angle))
         loss += tc.sum((image-image_sim)**2)
     
     return loss
-
-if __name__ == '__main__':
-    
-    import matplotlib.pyplot as plt
-
-    model = generate_gaussian_model(200, 30)
-    angle_rate = tc.Tensor([tc.pi/100])
-    
-    for k in range(100):
-
-        model2 = rotate_model(model, tc.Tensor([0,0,1]), k*angle_rate)
-        image = observe_model(model2, 0.05)
-    
-        plt.figure(1)
-        plt.clf()
-        plt.imshow(image)
-        plt.pause(0.05)
